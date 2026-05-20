be534b83b696b0cc73ea15cfe7f206a72d0af367
assignment-08 <sakilsikdar676@gmail.com>
2026-05-20 13:11:07 +0600

    booking post api and booking get api and booking delete api added

--- index.js
+++ index.js
@@ -4,11 +4,8 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 
-
-
 app.use(cors());
 app.use(express.json());
-
 
 const port = process.env.PORT || 8000;
 
@@ -30,6 +27,7 @@ async function run() {
   await client.connect();
 
   const carsCollection = client.db("DriveFleet").collection("cars");
+  const bookingCollection = client.db("DriveFleet").collection("booking");
   app.post("/cars", async (req, res) => {
     const car = req.body;
     const result = await carsCollection.insertOne(car);
@@ -40,7 +38,6 @@ async function run() {
     const result = await carsCollection.find().toArray();
     res.send(result);
   });
-
 
   app.get("/cars/:userId", async (req, res) => {
     const id = req.params.userId;
@@ -53,7 +50,7 @@ async function run() {
     const id = req.params.id;
     const status = req.body;
     console.log(status);
-    
+
     const query = { _id: new ObjectId(id) };
     const updateDoc = {
       $set: status,
@@ -69,9 +66,24 @@ async function run() {
     res.send(result);
   });
 
+  app.post("/booking", async (req, res) => {
+    const booking = req.body;
+    const result = await bookingCollection.insertOne(booking);
+    res.send(result);
+  });
 
+  app.get("/booking/:userId", async (req, res) => {
+    const { userId } = req.params;
+    const result = await bookingCollection.find({ userId: userId }).toArray();
+    res.send(result);
+  });
 
-
+  app.delete("/booking/:id", async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const result = await bookingCollection.deleteOne(query);
+    res.send(result);
+  });
 
   await client.db("admin").command({ ping: 1 });
   console.log("Pinged your deployment. You successfully connected to MongoDB!");
